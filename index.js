6b461a22409e60496789bbe9245d7cdfaa8cde3f
Dario Vladovic <d.vladimyr@gmail.com>
2019-11-09 23:16:25 +0100

    Lint codebase using ESLint + semistandard

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var leveljs = require('level-js');
 var levelup = require('levelup');
 var fs = require('level-filesystem');
 
-var db = levelup('level-filesystem', {db:leveljs});
+var db = levelup('level-filesystem', { db: leveljs });
 module.exports = fs(db);
