dcfe40ae39b15e54d0a2a7e2af3ba859a6c333be
Dario Vladovic <d.vladimyr@gmail.com>
2019-11-10 01:10:15 +0100

    Update example

--- example.js
+++ example.js
@@ -1,10 +1,14 @@
 'use strict';
 
-const fs = require('browserify-fs');
+global.window = global;
+require('fake-indexeddb/auto');
+
+const fs = require('.');
 
 fs.mkdir('/home', function () {
   fs.writeFile('/home/hello-world.txt', 'Hello world!\n', function () {
-    fs.readFile('/home/hello-world.txt', 'utf-8', function (_err, data) {
+    fs.readFile('/home/hello-world.txt', 'utf-8', function (err, data) {
+      if (err) return console.error(err);
       console.log(data);
     });
   });
